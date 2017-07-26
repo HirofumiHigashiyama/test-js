fa101df28d3d10f717f43f93fe9112de397f50ad
Hirofumi Higashiyama <hirofumihigashiyama@pc1.local>
2017-07-26 10:58:53 +0900

    ボタンをクリックしたら文字が変わる機能を実装した。

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function(){
   var btn = document.querySelector("input");
   btn.addEventListener("click", function(){
-  console.log("Hello World");
+    var p = document.querySelector("p");
+    p.innerText = "クリックされた！";
   });
 });
